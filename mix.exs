6e20c485d3f80fcf3becf5461ee14e698356be36
Sven Dittmer <sven.dittmer@gmx.net>
2018-11-04 20:50:51 +0100

    mix deps.get

--- mix.exs
+++ mix.exs
@@ -40,6 +40,7 @@ defmodule SeaTraders.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
+      {:plug_cowboy, "~> 1.0"},
       {:cowboy, "~> 1.0"},
       {:coherence, "~> 0.5"},
       {:bcrypt_elixir, "~> 1.0"},
